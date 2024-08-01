260dd5391bbd0cbc7b7597021289773dc1cd6c02
Andy Young <andy@mutinyhq.com>
2024-08-01 11:52:34 -0400

    feat: syncing nvim configs

---
+++ nvim/lua/plugins/cmp.lua
@@ -0,0 +1,40 @@
+return {
+  "hrsh7th/nvim-cmp",
+  -- load cmp on InsertEnter
+  event = "InsertEnter",
+  -- these dependencies will only be loaded when cmp loads
+  -- dependencies are always lazy-loaded unless specified otherwise
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "L3MON4D3/LuaSnip",
+  },
+  config = function()
+    local cmp = require("cmp")
+    cmp.setup({
+    snippet = {
+      -- REQUIRED - you must specify a snippet engine
+      expand = function(args)
+        require('luasnip').lsp_expand(args.body)
+      end,
+    },
+    window = {
+      completion = cmp.config.window.bordered(),
+      documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+      ['<C-n>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-p>'] = cmp.mapping.scroll_docs(4),
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-e>'] = cmp.mapping.abort(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    }),
+    sources = cmp.config.sources({
+      { name = 'nvim_lsp' },
+      { name = 'luasnip' }, -- For luasnip users.
+    }, {
+      { name = 'buffer' },
+    })
+  })
+  end,
+}
